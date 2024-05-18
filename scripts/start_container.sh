86dd17590edc93627a44736148ef04f8c725a5af
Adityakumardeshmukh <99591103+Adityakumardeshmukh@users.noreply.github.com>
2024-05-18 08:52:10 +0530

    change in image

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-sudo docker pull usigmoid/simple-python-flask-app
+sudo docker pull usigmoid/monitoring-app
 
 # Run the Docker image as a container
-sudo docker run -d -p 5000:5000 usigmoid/simple-python-flask-app
+sudo docker run -d -p 5000:5000 usigmoid/monitoring-app
